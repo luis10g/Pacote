601f6ab560e2f4e535672656ea4886e918e97af9
luis10g <joicefernandasouzaoliveira@gmail.com>
2022-04-11 17:52:29 +0000

    Pacote

---
+++ luisweb/Exemplo.java
@@ -0,0 +1,9 @@
+package luisweb;
+
+import luisweb.teste.Aluno
+public class Exemplo {
+    public static void main(String[] args) {
+        Aluno al = new Aluno();
+        System.out.println("Ola Pacote");
+    }
+}
